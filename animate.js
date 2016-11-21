fdab47631c10485bd74b4bf6c845bf761303bdd8
Daniil <dandonike@gmail.com>
2016-11-21 20:31:09 +0300

    убран jquery

--- animate.js
+++ animate.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-	$(".button").hover(function() {
-		$(this).stop().animate({backgroundColor: "#666"}, 100);
-	},function() {
-		$(this).stop().animate({backgroundColor: "#333"}, 100);
-	});
-	// $(".button").mousedown(function() {
-	// 	$(this).animate({marginTop: "155px", borderBottom: "0"}, 100);
-	// });
-	// $(".button").mouseup(function() {
-	// 	$(this).animate({marginTop: "150px", borderBottom: "5px solid #000"}, 100);
-	//  });
-});
+// $(document).ready(function() {
+// 	$(".button").hover(function() {
+// 		$(this).stop().animate({backgroundColor: "#666"}, 100);
+// 	},function() {
+// 		$(this).stop().animate({backgroundColor: "#333"}, 100);
+// 	});
+// 	$(".button").mousedown(function() {
+// 		$(this).animate({marginTop: "155px", borderBottom: "0"}, 100);
+// 	});
+// 	$(".button").mouseup(function() {
+// 		$(this).animate({marginTop: "150px", borderBottom: "5px solid #000"}, 100);
+// 	 });
+// });
 var butt = document.getElementById('butt');
 	butt.onmousedown = function(){
 		butt.style.marginTop = "155px";
